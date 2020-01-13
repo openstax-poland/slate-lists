6e8a833c337a80ea916773b3f475b5de31adb29d
Krzysztof Mędrzycki <aiwenari@gmail.com>
2020-01-13 11:05:37 +0100

    Initial

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import typescript from "@wessberg/rollup-plugin-ts"
+
+export default {
+    input: 'src/index.ts',
+    output: [
+        {
+            file: 'dist/index.es.js',
+            format: 'es',
+            sourcemap: true,
+        },
+        {
+            file: 'dist/index.cjs.js',
+            format: 'cjs',
+            sourcemap: true,
+            exports: 'named',
+        },
+    ],
+    plugins: [
+        typescript(),
+    ],
+}
