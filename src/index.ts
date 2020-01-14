de9eb63a8b4dde3291c6424adbb8178dca2b0fac
Krzysztof Mędrzycki <aiwenari@gmail.com>
2020-01-14 12:23:54 +0100

    Skeleton public API

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 // Copyright 2020 OpenStax Poland
 // Licensed under the MIT license. See LICENSE file in the project root for
 // full license text.
+
+import { Editor } from 'slate'
+
+/** A slate editor augmented with support for lists. */
+export interface ListEditor extends Editor {
+}
+
+/** Augment an editor with list related functionality and behaviours. */
+export function withLists<T extends Editor>(editor: T): T & ListEditor {
+    return editor
+}
