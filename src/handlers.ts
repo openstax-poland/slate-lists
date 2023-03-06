912fbe44b2553f8d06441e8e098781d5870d20a2
OneByteTwoByte <mateusz.tymiakin@openstax.pl>
2023-03-06 10:43:33 +0100

    enter in list upgraded
    no more empty list element
    
    works

--- src/handlers.ts
+++ src/handlers.ts
@@ -2,9 +2,10 @@
 // Licensed under the MIT license. See LICENSE file in the project root for
 // full license text.
 
-import { Editor, Transforms } from 'slate'
+import { Editor, NodeEntry, Transforms } from 'slate'
 
 import { List, ListItem } from './interfaces'
+import { decreaseDepth } from './transforms'
 
 /**
  * Handle a keydown event
@@ -31,8 +32,17 @@ function onEnter(editor: Editor, ev: KeyboardEvent): void {
         return
     }
 
+    if (!editor.selection) return
+
+    // splits node; if there was no text in a node, exits the list
     if (Editor.above(editor, { match: n => List.isList(n) })) {
+        const node: NodeEntry<any> = Editor.node(editor, editor.selection)
         Transforms.splitNodes(editor, { match: ListItem.isListItem, always: true })
+        if (node[0].text === '' || node[0].text ==='/n' || node[0].text === '/n/n') {
+            decreaseDepth(editor)
+            Transforms.removeNodes(editor,
+                { match: ListItem.isListItem, at: { path: node[1], offset: 0 } })
+        }
         return ev.preventDefault()
     }
 }
